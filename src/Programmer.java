8396d0022287d5a224348ed1b0b1c536a5bbb846
Alex <alex97av@gmail.com>
2023-07-27 17:20:47 +0200

    fine esercizio

--- src/Programmer.java
+++ src/Programmer.java
@@ -1,2 +1,19 @@
 public class Programmer {
+    private String name;
+    private int age;
+    private boolean wearGlasses;
+    public Programmer(String name,int age,boolean wearGlasses){
+        this.name=name;
+        this.age=age;
+        this.wearGlasses=wearGlasses;
+    }
+    public void drinkCoffe() {
+        System.out.println("Espresso is the secret!");
+    }
+    public void printDetails() {
+        System.out.println(name+" is a "+age+"-yo programmer");
+    }
+    public void hasGlasses() {
+        System.out.println("Is " +name+" wearing glasses? "+wearGlasses);
+    }
 }
--- src/TestProgrammers.java
+++ src/TestProgrammers.java
@@ -1,2 +1,10 @@
 public class TestProgrammers {
+public static void main(String[]args){
+    Programmer programmer1=new Programmer("Alessandro",26,false);
+    Programmer programmer2=new Programmer("Riccardo",32,true);
+    programmer1.drinkCoffe();
+    programmer1.printDetails();
+    programmer2.printDetails();
+    programmer2.hasGlasses();
+}
 }
